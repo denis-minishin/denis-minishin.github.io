d659f1875904c63ffb996f971ef6314141506dcb
Denis Minishin <denis.minishin@gmail.com>
2024-07-03 10:38:09 +0700

    del unnecessary favicons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,26 +24,6 @@ export default function Page() {
   const totalWorkExperienceRu = calculateTotalWorkExperienceRu(RESUME_DATA_RU.work);
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
-      <link rel="apple-touch-icon-precomposed" sizes="57x57" href="./images/apple-touch-icon-57x57.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="./images/apple-touch-icon-114x114.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="./images/apple-touch-icon-72x72.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="./images/apple-touch-icon-144x144.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="60x60" href="./images/apple-touch-icon-60x60.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="120x120" href="./images/apple-touch-icon-120x120.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="76x76" href="./images/apple-touch-icon-76x76.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="152x152" href="./images/apple-touch-icon-152x152.png" />
-      <link rel="icon" type="image/png" href="./images/favicon-196x196.png" sizes="196x196" />
-      <link rel="icon" type="image/png" href="./images/favicon-96x96.png" sizes="96x96" />
-      <link rel="icon" type="image/png" href="./images/favicon-32x32.png" sizes="32x32" />
-      <link rel="icon" type="image/png" href="./images/favicon-16x16.png" sizes="16x16" />
-      <link rel="icon" type="image/png" href="./images/favicon-128.png" sizes="128x128" />
-      <meta name="application-name" content="Денис Дмитриевич Минишин | Инженер-проектировщик АСУ ТП, КИПиА и диспетчеризации"/>
-      <meta name="msapplication-TileColor" content="#FFFFFF" />
-      <meta name="msapplication-TileImage" content="./images/mstile-144x144.png" />
-      <meta name="msapplication-square70x70logo" content="./images/mstile-70x70.png" />
-      <meta name="msapplication-square150x150logo" content="./images/mstile-150x150.png" />
-      <meta name="msapplication-wide310x150logo" content="./images/mstile-310x150.png" />
-      <meta name="msapplication-square310x310logo" content="./images/mstile-310x310.png" />
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-4">
         <div className="flex items-center justify-between">
           <div className="flex-1 space-y-1.5">
